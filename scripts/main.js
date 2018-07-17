e3eec8c90a51a4549fd72868984c7e827bb1ea91
majed187 <majedshrndan45@gmail.com>
2018-07-17 15:02:36 +0200

    added a new calss to set the event

--- scripts/main.js
+++ scripts/main.js
@@ -5,20 +5,41 @@
             this.baseURL = 'https://dog.ceo/api/';
         }
         getBreeds(){
-            return $.ajax(`${this.baseURL}breeds/list/all`).then(res => res.message);//.then(res=> console.log(api.baseURL));
-
-
+            return $.ajax(`${this.baseURL}breeds/list/all`).then(res => Object.keys(res.message)).catch(err => console.log(err));
         }
         getImage(breed){
-            return $.ajax(`${this.baseURL}breed/${breed}/images/random`).then(res => res.message);
+            return $.ajax(`${this.baseURL}breed/${breed}/images/random`).then(res => Object.keys(res.message)).catch(err => console.log(err));
+        }
+    }
+
+
+    class DogBrowserView {
+        constructor(selector){
+            this.root = document.body.querySelector(selector);
+        }
+
+        render(htmlString){
+            this.root.innerHTML = htmlString;
+        }
+        on(eventName,selector ,callback){
+
+            this.root.addEventListener(eventName, event => {
+
+                if(!event.target.matches(selector)) return;
+
+                callback(event);
+            });
         }
 
 
     }
-    const api = new DogApi();
-    console.log(api.baseUrl);
-    api.getBreeds().then(res => console.log(res));
-    api.getImage('african').then(res=> console.log(res)
-    );
+    const view = new DogBrowserView('#view');
+
+    console.log(view.root); // --> An html element for the root of our view
+    view.render('<p>test</p>');
+    console.log(view.root.innerHTML === '<p>test</p>');
+
+     view.on('click', 'p', event => console.log(event.target))
+
 
 })();
