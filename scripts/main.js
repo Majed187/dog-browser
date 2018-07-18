d6eb0117e8a29ea14bb4bf04671ed1f63988fe8f
majed187 <majedshrndan45@gmail.com>
2018-07-18 21:33:59 +0200

    added new Method to brint the carousel images

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,4 @@
 (function () {
-
     class DogApi {
         constructor() {
             this.baseURL = 'https://dog.ceo/api/';
@@ -11,54 +10,68 @@
             return $.ajax(`${this.baseURL}breed/${breed}/images/random`).then(res => res.message
             ).catch(err => console.log(err));
         }
+        getImages(breed){
+            return $.ajax(`${this.baseURL}breed/${breed}/images`).then(res => res.message
+            ).catch(err => console.log(err));
+        }
     }
 
 
     class DogBrowserView {
-        // create construtor method function with selector pram to make object inside the class
         constructor(selector){
-            // add proprty to the retrned object and make it equll the selctor in the html  page
             this.root = document.body.querySelector(selector);
         }
-        // set method that have parm on the class
         render(htmlString){
-            // take the proprty from the construtor and use the  inner html to maek it apper on the page
             this.root.innerHTML = htmlString;
         }
-        // on method that contain three pram 1: is the name of the event 2:
-        // is the element in the html page 3: is normal  callback function
         on(eventName,selector ,callback){
-            // in this line we take every element from the construtor and add the event on it
             this.root.addEventListener(eventName, event => {
-                // if element dose't match the selector don't do   any thing
                 if(!event.target.matches(selector)) return;
-                // we call the function with the name of the event
                 callback(event);
             });
         }
-
         breedListTemplate(templetes){
             const templete =templetes.map(templet =>
                 `
-               <button type="button" data-dog=${templet} class="list-group-item list-group-item-action mt-4 breed-button">${templet}</button>`).join('');
+               <button type="button" data-dog="${templet}" class="list-group-item list-group-item-action mt-4 breed-button">${templet}</button>`).join('');
 
             return`<div class="list-group">${templete}</div>`;
         }
-
         dogImageTemplate(img){
             return `
-           <header>
-               <button type="button" id="submit-button" class="btn btn-primary back-button">Go Back</button>
-           </header>
-   <section>
-      <img class="img-fluid" src="${img}">
-   </section>`;
+         <header>
+            <button type="button" id="submit-button" class="btn btn-primary back-button mb-3 mt-4">Go Back!!</button>
+         </header>
+
+            <section class="carousel-inner">
+               <img class="img-fluid w-80" src="${img}">
+               </section>`;
+        }
+        dogImagesTemplate(images){
+            const carousel = images.map((img , i) => `
+               <div class="carousel-item ${i===0? 'active':''}">
+               <img class="img-fluid w-100" src="${img}">
+               </div>`).join('');
+            return `
+            <header>
+               <button type="button" id="submit-button" class="btn btn-primary back-button btn-lg btn-block mb-3 mt-4">Go Back!!</button>
+            </header>
+               <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                  <div class="carousel-inner">
+                  ${carousel}
+                  </div>
+                  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Next</span>
+                  </a>
+               </div> `
         }
     }
 
-
-
-    // creat an a instance of the class DogBrowserView and set the id on it
     const view = new DogBrowserView('#view');
     const api = new  DogApi ();
 
@@ -75,4 +88,24 @@
             view.render(view.dogImageTemplate(image));
         });
     });
+
+    view.on('click', '.breed-button' , event => {
+        event.preventDefault();
+        const breed = event.target.dataset.dog;
+        api.getImages(breed).then(image =>{
+            view.render(view.dogImagesTemplate(image));
+        });
+    });
+
+
+    view.on('click','#submit-button',(event) => {
+        event.preventDefault();
+        const button = event.target;
+        api.getBreeds().then(words =>{
+            view.render(view.breedListTemplate(words));
+        });
+    });
+
+
+    //api.getImages('african').then(console.log)
 })();
